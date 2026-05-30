c17b7f5b7ee024cb463f942411f168ebc4ae0153
fontesyasmim768-cmd <fontesyasmim768@gmail.com>
2026-05-30 01:25:50 +0000

    massa

--- react_vazio/vazio/App.js
+++ react_vazio/vazio/App.js
@@ -4,8 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.viewred}>
+        <Text style={styles.red}>Studio de Cilios</Text>
+      </View>
+
+      <View style={styles.viewred}>
+        <Text style={styles.bigBlue}>Realce sua Beleza</Text>
+      </View>
+
+      <View style={styles.viewred}>
+        <Text style={[styles.bigBlue, styles.red]}>
+          Alongamento de Cilios
+        </Text>
+      </View>
+
+      <View style={styles.viewred}>
+        <Text style={[styles.red, styles.bigBlue]}>
+          Agende seu Horario!
+        </Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +30,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#F8F6F4',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+
+  viewred: {
+    width: '90%',
+    backgroundColor: '#FFFFFF',
+    padding: 20,
+    marginVertical: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    elevation: 5,
+  },
+
+  bigBlue: {
+    fontSize: 28,
+    fontWeight: '700',
+    color: '#2D2D2D',
+    textAlign: 'center',
+  },
+
+  red: {
+    fontSize: 20,
+    color: '#ff001e',
+    textAlign: 'center',
+    fontWeight: '600',
   },
 });
